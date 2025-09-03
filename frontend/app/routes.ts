15766d5bd451422f72cb4cb20602f9a612f32080
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-03 11:48:34 -0500

    add authentication routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,13 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("./routes/root/home.tsx")] satisfies RouteConfig;
+export default [
+    layout("routes/auth/auth-layout.tsx",[
+        index("routes/root/home.tsx"),
+        route("sign-in","routes/auth/sign-in.tsx"),
+        route("sign-up","routes/auth/sign-up.tsx"),
+        route("forgot-password.tsx","routes/auth/forgot-password.tsx"),
+        route("reset-password.tsx","routes/auth/reset-password.tsx"),
+        route("verify-email.tsx","routes/auth/verify-email.tsx"),
+
+    ]),
+] satisfies RouteConfig;
