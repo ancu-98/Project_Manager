09d35410be3c27f7cc55f6936e7ebc2c40989f37
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-03 11:46:06 -0500

    add index page

--- frontend/app/routes/root/home.tsx
+++ frontend/app/routes/root/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type { Route } from '../../+types/root';
-import { Button } from '~/components/ui/button';
+import { Link } from 'react-router';
+import { Button } from '@/components/ui/button';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,7 +12,14 @@ export function meta({}: Route.MetaArgs) {
 
 const HomePage = () => {
   return (
-    <Button>CLickMe</Button>
+    <div className='w-full h-screen flex items-center justify-center gap-4'>
+      <Button asChild className='bg-blue-500 text-white'>
+        <Link to='/sign-in'> Login </Link>
+      </Button>
+      <Button asChild variant='outline' className='bg-blue-500 text-white'>
+        <Link to='/sign-up'> Sign Up </Link>
+      </Button>
+    </div>
   )
 }
 
