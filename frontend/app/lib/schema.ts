190c3f610531cf199bd977348447d384dac3a95e
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-05 11:09:26 -0500

    update sing-up schema

--- frontend/app/lib/schema.ts
+++ frontend/app/lib/schema.ts
@@ -1,9 +1,19 @@
-import { z, ZodString } from 'zod';
+import { z } from 'zod';
 
 export const signInSchema = z.object({
-    email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password required"),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password required'),
 });
 
-
-
+export const signUpSchema = z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(8, 'Password must be 8 characters'),
+    confirmPassword: z.string().min(8, 'Password must be 8 characters'),
+    name: z.string().min(3, 'Name must be at least 3 characters'),
+    university: z.string().max(50, 'University cannot exceed 50 characters').nonempty,
+    career: z.string().max(50, 'Career cannot exceed 50 characters').nonempty,
+    currentSemester: z.int().min(1).max(12),
+}).refine((data) => data.password === data.confirmPassword,{
+    path: ['confirmPassword'],
+    message:'Password do not match',
+});
