78965923b5adc6c34f6f1a008d554e7797c116e8
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-05 11:07:54 -0500

    add authentication components

--- frontend/app/routes/auth/sign-in.tsx
+++ frontend/app/routes/auth/sign-in.tsx
@@ -1,105 +1,126 @@
-import { signInSchema } from '@/lib/schema';
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Link } from 'react-router';
+import { signInSchema } from "@/lib/schema";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
-type SignInFormData = z.infer<typeof signInSchema>
+type SignInFormData = z.infer<typeof signInSchema>;
 
 const SignIn = () => {
+  const form = useForm<SignInFormData>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const form = useForm<SignInFormData>({
-        resolver: zodResolver(signInSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-
-    });
-
-    const handleOnsubmit = (values: SignInFormData) => {
-        console.log(values)
-    }
+  const handleOnsubmit = (values: SignInFormData) => {
+    console.log(values);
+  };
 
   return (
-    <div className='min-h-screen min-w-screen flex flex-col items-center justify-center bg-gray-600 p-4' >
-        <Card className='max-w-md w-auto bg-gray-950 shadow-accent'>
-            <CardHeader className='text-center mb-5' >
-                <CardTitle className='text-2x1 font-bold shadow-x1 text-white'>
-                    Welcome back
-                </CardTitle>
-                <CardDescription className='text-sm text-muted-foreground'>
-                    Sign in to your account to continue
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form
-                        onSubmit={form.handleSubmit(handleOnsubmit)}
-                        className='space-y-6'
-                    >
-                        <FormField
-                            control={form.control}
-                            name="email"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel className='text-2x1 font-bold shadow-x1 text-white'>
-                                        Email
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type='email'
-                                            placeholder='email@exmaple.com'
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-                        <FormField
-                            control={form.control}
-                            name="password"
-                            render={({field}) => (
-                                <FormItem>
-                                    <FormLabel className='text-2x1 font-bold shadow-x1 text-white'>
-                                        Password
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type='password'
-                                            placeholder='*********'
-                                            {...field}
-                                        />
-                                    </FormControl>
-                                    <FormMessage/>
-                                </FormItem>
-                            )}
-                        />
-
-                        <Button type='submit' className='w-full'>
-                            Sign In
-                        </Button>
-
-                    </form>
-                </Form>
-
-                <CardFooter>
-                    <div className='m-1 flex items-center justify-center'>
-                        <p className='text-sm text-muted-foreground' >
-                            Don&apos;t have an account? <Link to='/sign-up' className='text-blue-400'>Sign up</Link>
-                        </p>
+    <div className="min-h-screen min-w-screen flex flex-col items-center justify-center bg-gray-600 p-4">
+      <Card className="max-w-md w-auto bg-gray-950 shadow-accent">
+        <CardHeader className="text-center mb-5">
+          <CardTitle className="text-2x1 font-bold shadow-x1 text-white">
+            Welcome back
+          </CardTitle>
+          <CardDescription className="text-sm text-muted-foreground">
+            Sign in to your account to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(handleOnsubmit)}
+              className="space-y-6"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                      Email
+                    </FormLabel>
+                    <FormControl className="text-muted-foreground">
+                      <Input
+                        type="email"
+                        placeholder="email@exmaple.com"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center justify-between">
+                      <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                        Password
+                      </FormLabel>
+                      <Link
+                        to="/forgot-password"
+                        className="text-sm text-blue-600"
+                      >
+                        forgot password?
+                      </Link>
                     </div>
-                </CardFooter>
-            </CardContent>
-        </Card>
+                    <FormControl className="text-muted-foreground">
+                      <Input
+                        type="password"
+                        placeholder="*********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button type="submit" className="w-full">
+                Sign In
+              </Button>
+            </form>
+          </Form>
+
+          <CardFooter className="flex items-center justify-center nt-6">
+            <div className="mt-5 flex items-center justify-center">
+              <p className="text-sm text-muted-foreground">
+                Don&apos;t have an account?{" "}
+                <Link to="/sign-up" className="text-blue-400">
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </CardFooter>
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
 export default SignIn;
---
+++ frontend/app/routes/auth/sign-up.tsx
@@ -0,0 +1,304 @@
+import { signUpSchema } from "@/lib/schema";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { int, z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
+type SignUpFormData = z.infer<typeof signUpSchema>;
+
+const SignUp = () => {
+  const form = useForm<SignUpFormData>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      name: "",
+      university: "",
+      career: "",
+      currentSemester: 1,
+    },
+  });
+
+  const handleOnsubmit = (values: SignUpFormData) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-600 p-4">
+      <Tabs defaultValue="account" className="w-full max-w-sm sm:max-w-md md:max-w-lg">
+        <TabsList className="bg-gray-950">
+          <TabsTrigger
+            value="account"
+            className="data-[state=active]:bg-gray-500/20 data-[state=active]:border-gray-400/20 data-[state=active]:text-white text-muted-foreground"
+          >
+            Account
+          </TabsTrigger>
+          <TabsTrigger
+            value="university"
+            className="data-[state=active]:bg-gray-500/20 data-[state=active]:border-gray-400/20 data-[state=active]:text-white text-muted-foreground"
+          >
+            University
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="account" className="w-auto">
+          <Card className="w-full max-w-sm sm:max-w-md bg-gray-950 shadow-accent">
+            <CardHeader className="text-center sm:mb-5 px-4 sm:px-6" >
+              <CardTitle className="text-lg sm:text-xl md:text-2xl font-bold text-white">
+                Create an acount
+              </CardTitle>
+              <CardDescription className="text-sm text-muted-foreground">
+                Sign in to your account to continue
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-4 sm:px-6">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(handleOnsubmit)}
+                  className="space-y-4 sm:space-y-6"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-2x1 font-bold shadow-x1">
+                          Email
+                        </FormLabel>
+                        <FormControl className="text-muted-foreground">
+                          <Input
+                            className="text-sm sm:text-base"
+                            type="email"
+                            placeholder="email@exmaple.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                          Password
+                        </FormLabel>
+                        <FormControl className="text-muted-foreground">
+                          <Input
+                            className="text-sm sm:text-base"
+                            type="password"
+                            placeholder="*********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                          Confirm Password
+                        </FormLabel>
+                        <FormControl className="text-muted-foreground">
+                          <Input
+                            className="text-sm sm:text-base"
+                            type="password"
+                            placeholder="*********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <Button type="submit" className="w-full py-2 sm:py-3 text-sm sm:text-base">
+                    Sign Up
+                  </Button>
+                </form>
+              </Form>
+
+              <CardFooter className="flex items-center justify-center nt-6">
+                <div className="mt-5 flex items-center justify-center">
+                  <p className="text-sm text-muted-foreground">
+                    Already have an account?{" "}
+                    <Link to="/sign-in" className="text-blue-400">
+                      Sign in
+                    </Link>
+                  </p>
+                </div>
+              </CardFooter>
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="university" className="">
+          <Card className="w-full max-w-sm sm:max-w-md bg-gray-950 shadow-accent">
+            <CardHeader className="text-center mb-3 sm:mb-5 px-4 sm:px-6">
+              <CardTitle className="text-lg sm:text-xl md:text-2xl font-bold text-white">
+                Create an acount
+              </CardTitle>
+              <CardDescription className="text-sm text-muted-foreground">
+                Sign up to your account to continue
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="px-4 sm:px-6">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(handleOnsubmit)}
+                  className="space-y-4 sm:space-y-6"
+                >
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                          Full Name
+                        </FormLabel>
+                        <FormControl className="text-muted-foreground" >
+                          <Input
+                            className="text-sm sm:text-base"
+                            type="text"
+                            placeholder="Jhon Doe"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="university"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                          University
+                        </FormLabel>
+                        <FormControl className="text-muted-foreground" >
+                          <Input
+                            type="text"
+                            placeholder="Universidad de Cundinamarca"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="flex items-end space-x-2 min-w-0">
+                    <FormField
+                      control={form.control}
+                      name="career"
+                      render={({ field }) => (
+                        <FormItem className="flex-1 min-w-0" >
+                          <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                            Carreer
+                          </FormLabel>
+                          <FormControl className="text-muted-foreground">
+                            <Input
+                                className="text-sm sm:text-base w-full"
+                              type="text"
+                              placeholder="Ingenieria de Sistemas"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="currentSemester"
+                      render={({ field }) => (
+                        <FormItem className="flex-shrink-0 min-w-[60px] sm:w-[100px]" >
+                          <FormLabel className="text-2x1 font-bold shadow-x1 text-muted-foreground">
+                            Semester
+                          </FormLabel>
+                          <FormControl>
+                            <Select onValueChange={(value) => field.onChange(parseInt(value, 10))} value={field.value?.toString() || ""}>
+                              <SelectTrigger className="w-full border-gray-200/20 text-muted-foreground">
+                                <SelectValue placeholder="Select" className="text-muted-foreground" />
+                              </SelectTrigger>
+                              <SelectContent className="bg-gray-950/90 border-gray-200/20 max-h-[280px] overflow-y-auto">
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white" value="1">1</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="2">2</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="3">3</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="4">4</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="5">5</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="6">6</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="7">7</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="8">8</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="9">9</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="10">10</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="11">11</SelectItem>
+                                <SelectItem className="text-muted-foreground focus:border-gray-400/20 focus:bg-gray-500/20 focus:text-white"value="12">12</SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                </form>
+              </Form>
+
+              <CardFooter className="flex items-center justify-center nt-6">
+                <div className="mt-5 flex items-center justify-center">
+                  <p className="text-sm text-muted-foreground">
+                    Already have an account?{" "}
+                    <Link to="/sign-in" className="text-blue-400">
+                      Sign in
+                    </Link>
+                  </p>
+                </div>
+              </CardFooter>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default SignUp;
