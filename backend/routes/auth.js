069de5ea0a91e867ad9dbb27d94af621a3cc1d6e
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-20 22:43:17 -0500

    create /verify-email route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {z} from 'zod'
 import { validateRequest } from 'zod-express-middleware';
-import { loginSchema, registerSchema } from '../libs/validate.schema.js';
-import { loginUser, registerUser } from '../controllers/auth.controller.js';
+import { loginSchema, registerSchema, verifyEmailSchema } from '../libs/validate.schema.js';
+import { loginUser, registerUser, verifyEmail } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -23,5 +23,13 @@ router.post(
     }),
     loginUser
 );
+
+router.post(
+    '/verify-email',
+    validateRequest({
+        body:verifyEmailSchema,
+    }),
+    verifyEmail
+)
 
 export default router;
