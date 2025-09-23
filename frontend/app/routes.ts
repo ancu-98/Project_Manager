e753560317f43e6707951df3683200f23cca0ec2
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-23 13:09:52 -0500

    add dashboard routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -10,4 +10,9 @@ export default [
         route("verify-email","routes/auth/verify-email.tsx"),
 
     ]),
+    layout("routes/dashboard/dashboard-layout.tsx", [
+        route("dashboard","routes/dashboard/app.tsx"),
+
+    ]),
+
 ] satisfies RouteConfig;
