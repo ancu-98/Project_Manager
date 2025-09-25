bc253a1a98b5af41b201057adf2912419bce8732
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-25 18:22:20 -0500

    create user & workspace interface

--- frontend/app/types/app.ts
+++ frontend/app/types/app.ts
@@ -4,6 +4,25 @@ export interface User {
     name: string;
     university: string;
     career: string;
-    currentSemester: string
+    currentSemester: string;
     createdAt: Date;
+    isEmailVerified: boolean;
+    updatedAt: Date;
+    profilePicture: string
 }
+
+export interface Workspace {
+    _id: string;
+    name: string;
+    description?: string;
+    owner: User | string;
+    color: string;
+    members: {
+        user: User;
+        role: 'admin' | 'member' | 'owner' | 'viewer';
+        joinetAt: Date;
+    }[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
