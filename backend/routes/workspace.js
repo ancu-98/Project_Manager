ea1b3fa747ba6903068f48bae2cae0135f8fb55b
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-29 20:30:38 -0500

    add get workspaces route

--- backend/routes/workspace.js
+++ backend/routes/workspace.js
@@ -2,8 +2,7 @@ import express from 'express';
 import { validateRequest } from 'zod-express-middleware';
 import { workspaceSchema } from '../libs/validate.schema.js';
 import authMiddleware from '../middleware/auth.middleware.js';
-import { createWorkspace } from '../controllers/workspace.js'
-
+import { createWorkspace, getWorkspaces } from '../controllers/workspace.js'
 
 const router = express.Router();
 
@@ -13,5 +12,7 @@ router.post(
     validateRequest({body: workspaceSchema}),
     createWorkspace
 );
+
+router.get('/', authMiddleware, getWorkspaces);
 
 export default router;
