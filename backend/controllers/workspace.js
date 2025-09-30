e55678acdbf2444931688ed2e793c04700a5798c
ancu-98 <jdsebastianb30@hotmail.com>
2025-09-29 20:29:17 -0500

    add get workspaces controller

--- backend/controllers/workspace.js
+++ backend/controllers/workspace.js
@@ -1,6 +1,6 @@
 import Workspace from "../models/workspace.js";
 
-export const createWorkspace = async (req, res) => {
+const createWorkspace = async (req, res) => {
   try {
     const { name, description, color } = req.body;
     const workspace = await Workspace.create({
@@ -26,3 +26,19 @@ export const createWorkspace = async (req, res) => {
     });
   }
 };
+
+const getWorkspaces = async (req, res) => {
+    try {
+        const workspaces = await Workspace.find({
+            'members.user': req.user._id,
+        }).sort({ createdAt: -1});
+
+        res.status(200).json(workspaces)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: 'Internal Server error'})
+    }
+}
+
+export {createWorkspace, getWorkspaces};
