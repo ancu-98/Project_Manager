ec59f0959e20d329772cb8e759795c2c415d089a
ancu-98 <jdsebastianb30@hotmail.com>
2025-10-05 17:09:13 -0500

    add getWorkspaceDetails & getWorkspaceProjects controllers

--- backend/controllers/workspace.js
+++ backend/controllers/workspace.js
@@ -1,4 +1,5 @@
 import Workspace from "../models/workspace.js";
+import Project from '../models/project.js'
 
 const createWorkspace = async (req, res) => {
   try {
@@ -41,4 +42,64 @@ const getWorkspaces = async (req, res) => {
     }
 }
 
-export {createWorkspace, getWorkspaces};
+const getWorkspaceDetails = async (req, res) => {
+  try {
+    const { workspaceId } = req.params;
+
+    const workspace = await Workspace.findOne({
+      _id: workspaceId,
+      'members.user': req.user._id,
+    }).populate('members.user', 'name email profilePicture');
+
+    if (!workspace) {
+      return res.status(404).json({
+        message: 'Workspace not found',
+      });
+    }
+
+    res.status(200).json(workspace);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Internal Server error'})
+  }
+}
+
+const getWorkspaceProjects = async (req, res) => {
+  try {
+    const { workspaceId } = req.params;
+
+    const workspace = await Workspace.findOne({
+      _id: workspaceId,
+      'members.user': req.user._id,
+    }).populate('members.user', 'name email profilePicture');
+
+    if (!workspace) {
+      return res.status(404).json({
+        message: 'Workspace not found',
+      });
+    }
+
+    const projects = await Project.find({
+      workspace: workspaceId,
+      isArchived: false,
+      members: { $in: [req.user._id] },
+    })
+      .populate('activities', 'status')
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({projects, workspace});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Internal Server error'})
+  }
+
+}
+
+export {
+  createWorkspace,
+  getWorkspaces,
+  getWorkspaceDetails,
+  getWorkspaceProjects,
+};
