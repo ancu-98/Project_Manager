a9b4ea751f091b4ba07032cc8de6996388b14c41
ancu-98 <jdsebastianb30@hotmail.com>
2025-10-21 18:13:06 -0500

    add activity and sprint hooks

--- frontend/app/hooks/use-project.ts
+++ frontend/app/hooks/use-project.ts
@@ -1,6 +1,6 @@
 import type { CreateProjectFormData } from "@/components/project/create-project";
-import { postData } from "@/lib/fetch-util";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { fetchData, postData } from "@/lib/fetch-util";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const UseCreateProject = () => {
   const queryClient = useQueryClient();
@@ -17,6 +17,34 @@ export const UseCreateProject = () => {
     onSuccess: (data: any) => {
       queryClient.invalidateQueries({
         queryKey: ["workspace", data.workspace],
+      });
+    },
+  });
+};
+
+
+export const useProjectBacklogActivitiesQuery = (projectId: string) => {
+  return useQuery({
+    queryKey: ['project', projectId],
+    queryFn: () => fetchData(`/projects/${projectId}/backlog/activities`)
+  })
+}
+
+export const useReturnSprintActivitytoProjectBacklogMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: {
+      projectId: string;
+      activityId: string;
+    }) =>
+      postData(
+        `/projects/${data.projectId}/backlog/return-activity/${data.activityId}`,
+        {}
+      ),
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["project", variables.projectId],
       });
     },
   });
