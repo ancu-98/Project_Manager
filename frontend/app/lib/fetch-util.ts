70f995b4efa2494738033512e0662a7f0575ab5c
ancu-98 <jdsebastianb30@hotmail.com>
2025-11-14 01:03:29 -0500

    update base url for production

--- frontend/app/lib/fetch-util.ts
+++ frontend/app/lib/fetch-util.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
